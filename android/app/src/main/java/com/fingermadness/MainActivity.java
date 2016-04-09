23a7de61d73a47731e4fdf38a8dfd8515f9b01b2
Kimmo Yli-Rohdainen <kimmo.yli-rohdainen@vincit.com>
2016-04-09 21:52:07 +0300

    Add commented out immersive mode support (does not work, touch events are misplaced).

--- android/app/src/main/java/com/fingermadness/MainActivity.java
+++ android/app/src/main/java/com/fingermadness/MainActivity.java
@@ -3,11 +3,38 @@ package com.fingermadness;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.bridge.UiThreadUtil;
 
 import java.util.Arrays;
 import java.util.List;
 
+import android.view.View;
+import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+
 public class MainActivity extends ReactActivity {
+
+    /*
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UiThreadUtil.runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        getWindow().getDecorView().setSystemUiVisibility(
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+                        );
+                        ViewCompat.requestApplyInsets(getWindow().getDecorView());
+                    }
+                }
+        );
+    }
+    */
 
     /**
      * Returns the name of the main component registered from JavaScript.
